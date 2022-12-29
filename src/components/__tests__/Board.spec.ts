f07954607b7d0e5f6bc7791b3b3d2f203cd35697
aoi <aoi@MacBook-Pro.local>
2022-12-29 17:00:22 +0900

    first vite test

---
+++ src/components/__tests__/Board.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Board from '@/views/Board.vue';
+
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    const wrapper = mount(Board)
+    expect(wrapper.text()).toContain('Hello World')
+  })
+})
